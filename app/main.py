b91de336be67d1de2effe572895ab67f0357ad01
lubkoserhii <lubko.serhii@gmail.com.com>
2026-04-21 16:47:02 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,40 @@
-def shop_trip():
-    # write your code here
-    pass
+from app.config_loader import load_data
+
+
+def shop_trip() -> None:
+    fuel_price, customers, shops = load_data()
+
+    for customer in customers:
+        print(f"{customer.name} has {customer.money} dollars")
+        cheapest_shop = None
+        cheapest_trip_cost = None
+
+        for shop in shops:
+            trip_cost = customer.calculate_trip_cost(shop, fuel_price)
+            print(
+                f"{customer.name}'s trip to the {shop.name} "
+                f"costs {trip_cost:.2f}"
+            )
+
+            if cheapest_trip_cost is None or trip_cost < cheapest_trip_cost:
+                cheapest_trip_cost = trip_cost
+                cheapest_shop = shop
+
+        if (
+            cheapest_trip_cost is None
+            or not customer.can_afford(cheapest_trip_cost)
+        ):
+            print(
+                f"{customer.name} doesn't have enough money "
+                "to make a purchase in any shop"
+            )
+            continue
+
+        print(f"{customer.name} rides to {cheapest_shop.name}")
+        customer.ride_to(cheapest_shop.location)
+        cheapest_shop.print_receipt(customer.name, customer.product_cart)
+        print(f"{customer.name} rides home")
+        customer.ride_to([0, 0])
+        customer.spend_money(cheapest_trip_cost)
+        print(f"{customer.name} now has {customer.money:.2f} dollars")
+        print()
